d38bd9462673a6978902467b957e185c51cfe797
vladimir9961 <47478598+vladimir9961@users.noreply.github.com>
2020-10-02 16:09:10 +0200

    Add files via upload

--- ToDoApp/js.js
+++ ToDoApp/js.js
@@ -8,55 +8,72 @@ input.addEventListener("keyup", function(event) {
   }
 });
 
-
-function empty(){
+function information(){
   // Selecting the input element and get its value 
   if(document.getElementById("input").value.length == 0)
   {
-      alert("Cant leave empty field")
+      alert("NEMOZE PRAZNO POLJE!!!");  
+
   } else{
     let inputVal = document.getElementById("input").value;
-
+    
     // Appending the value from input in created element
     let newTH = document.createElement('li');      
     newTH.setAttribute("id", "list");
 
     //Creating paragraf inside li
     let newP = document.createElement('p'); 
+    newP.setAttribute("id", "paragraf");
     newTH.appendChild(newP);
     newP.innerHTML = inputVal;
-     
+
     //Creating button for delete
     let newBtn = document.createElement('button');
     newBtn.setAttribute("id", "btn"); 
-    newBtn.innerHTML = "Done";
+    newBtn.innerHTML = "Delete";
     newTH.appendChild(newBtn);
+
+    //Onclick btn delete li element
+    newBtn.onclick = function() {
+      let delBtn = newTH;
+      delBtn.remove();
+    };
+
+    //Creating button for done
+    let btnDone = document.createElement('button');
+    btnDone.setAttribute("id", "btnDone"); 
+    btnDone.innerHTML = "Done";
+    newTH.appendChild(btnDone);
+    
+    //toggle to set styles NOT FINISHED
+    btnDone.onclick = function () {
+      let element = newP;
+      element.classList.toggle("line");
+   }
+
+    //Appends dates
+    const inputTime = new Date().toISOString().slice(0, 10);
+    let pTime = document.createElement('p');
+    pTime.setAttribute("id", "pTime"); 
+    pTime.innerHTML = inputTime;
+    newTH.appendChild(pTime);
 
     //Creating li
     let table = document.getElementById('todo-list');
     table.appendChild(newTH);
-    
-   
-
-    newBtn.onclick = function() {
-      let delBtn = newTH;
-      delBtn.remove();
-      
-      
-    };
-   
   };
-  document.getElementById('input').value = ''
+
+  document.getElementById('input').value = '';
+
 };
 
-//Button For Deleting Items From Li
+//Button For Deleting All Items from Ul
 function myFunction() {
-  let e = document.querySelector("ul"); 
-        
+  let e = document.querySelector("ul");        
   let child = e.lastElementChild;  
   while (child) { 
       e.removeChild(child); 
-      child = e.lastElementChild;
-    
-}
+      child = e.lastElementChild; 
+  }
 };
+
